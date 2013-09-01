59a9d8fc035739b9b1e31c9fda15752bc00e07e9
Robert Rossmann <rr.rossmann@me.com>
2013-09-02 00:54:22 +0200

    [dev] Improve inline documentation

--- Ldap/Response.php
+++ Ldap/Response.php
@@ -29,11 +29,11 @@ namespace Ldap;
  * @property-read		array			$data		Only available when a resource is available - The data extracted from a resource
  * @property-read		int				$code		A status code of the ldap operation executed
  * @property-read		string			$message	A status message associated with the status code
+ * $property-read		array			$referrals	If the server responds with referrals, you will find them here
  * @property-read		binary			$cookie		For paged result responses, a cookie will be here, if returned from server
- * $property-read		array			$referrals	When checking for referrals,
- *
  * @property-read		int				$estimated	The estimated number of objects remaining to return from server
  * 													when doing paged searches ( not all ldap implementations return this value )
+ * @property-read		string			$matchedDN	Not much is known here; read php's documentation about ldap_parse_result()
  */
 class Response
 {
@@ -41,10 +41,11 @@ class Response
 	protected $data;				// The actual ldap data extracted from result, in case a resource was returned
 	protected $code;				// Status code of the operation
 	protected $message;				// Textual representation of the status code
-	protected $referrals;
-	protected $matchedDN;
+	protected $referrals;			// List of returned referrals in the resultset ( if any )
 	protected $cookie;				// A pagination cookie if returned from server
 	protected $estimated;			// An estimated number of objects yet to be returned from server for paged searches
+	protected $matchedDN;			// Purpose unknown; available for compatibility reasons
+
 
 	public function __construct( Ldap $link, $result = null )
 	{
@@ -52,11 +53,17 @@ class Response
 
 		if ( is_resource( $result ) )
 		{
+			// Extract the data from the resource
 			$this->data = ldap_get_entries( $link->resource(), $result );
-			ldap_parse_result( $link->resource(), $result, $this->code, $this->matchedDN, $this->message, $this->referrals );
-			$this->message = ldap_error( $link->resource() );
 
-			( count( $this->referrals ) == 0 ) && $this->referrals = null;	// Remove the array if there's nothing inside
+			// Get the status code, matched DN and referrals from the response
+			ldap_parse_result( $link->resource(), $result, $this->code, $this->matchedDN, $this->message, $this->referrals );
+
+			// Get the string representation of the status code
+			$this->message = ldap_err2str( $this->code );
+
+			// Remove the referrals array if there's nothing inside
+			( count( $this->referrals ) == 0 ) && $this->referrals = null;
 
 			// Try to extract pagination cookie and estimated number of objects to be returned
 			// Since there's no way to tell if pagination has been enabled or not, I am suppressing php errors
